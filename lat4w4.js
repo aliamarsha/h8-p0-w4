823645c47f4cf3052cd40cd20cf06f6a82b1844a
aliamarsha <aliamarsha96@gmail.com>
2019-12-05 16:04:36 +0700

    latihan 4 cari modus

--- lat4w4.js
+++ lat4w4.js
@@ -1,7 +1,34 @@
-// Mencari Modus BELUM SELESAI
+// Mencari Modus
 
 function cariModus(arr) {
+  var penampung = []
 
+  for (var i=0; i < arr.length; i++){
+    cek = false;
+    for (var j=0; j < penampung.length; j++){
+      if (arr[i] == penampung[j][0]){
+        penampung[j][1] += 1; 
+      }
+    }
+    if (cek == false){
+      penampung.push([arr[i], 1])
+    }
+  }
+  var pembanding = 0; 
+  var lokasiModus = 0; 
+
+  for (var k = 0; k < penampung.length; k++){
+    if (pembanding < penampung[k][1]){
+      pembanding = penampung[k][1]; 
+      lokasiModus = k; 
+    } 
+  }
+
+  if (pembanding == 1 || penampung[lokasiModus][1] == arr.length){
+    return -1
+  }
+  //console.log(lokasiModus)
+  return penampung[lokasiModus][0];
   }
   
   // TEST CASES
