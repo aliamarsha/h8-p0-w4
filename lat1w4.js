7284f0cd645a7a18bbd807c5759d7040ad6b0cb3
aliamarsha <41851802+aliamarsha@users.noreply.github.com>
2019-12-05 16:39:43 +0700

    Update lat1w4.js

--- lat1w4.js
+++ lat1w4.js
@@ -24,4 +24,4 @@ console.log(angkaPrima(3)); // true
 console.log(angkaPrima(7)); // true
 console.log(angkaPrima(6)); // false
 console.log(angkaPrima(23)); // true
-console.log(angkaPrima(33)); // false --> CUMA INI SALAH
+console.log(angkaPrima(33)); // false 
