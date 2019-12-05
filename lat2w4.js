d61bfac3ad4618d74ff96000f7377c67345021f9
aliamarsha <41851802+aliamarsha@users.noreply.github.com>
2019-12-05 16:38:58 +0700

    Update lat2w4.js

--- lat2w4.js
+++ lat2w4.js
@@ -16,4 +16,4 @@ function fpb(angka1, angka2) {
   console.log(fpb(50, 40)); // 10
   console.log(fpb(22, 99)); // 11
   console.log(fpb(24, 36)); // 12
-  console.log(fpb(17, 23)); // 1 --> CUMA INI SALAH. HASIL MALAH 6
+  console.log(fpb(17, 23)); // 1 
