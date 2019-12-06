d3077656f26fd0587d32a71569e95ad4a4a5023f
aliamarsha <41851802+aliamarsha@users.noreply.github.com>
2019-12-06 14:42:51 +0700

    Update lat6w4.js

--- lat6w4.js
+++ lat6w4.js
@@ -1,7 +1,29 @@
-// Digit Perkalian Minimum BELUM SELESAI
+// Digit Perkalian Minimum
 
 function digitPerkalianMinimum(angka) {
-    // you can only write your code here!
+    var faktor1 = 0; 
+    var faktor2 = 0; 
+    var faktorStr1 = 0;
+    var faktorStr2 = 0;
+    var lowest = 0; 
+
+    for (var i = 0; i <= angka; i++){
+      if (angka % i === 0){
+        faktor1 = i
+        faktor2 = angka / i
+        faktorStr1 = faktor1.toString().length
+        faktorStr2 = faktor2.toString().length
+      }
+      var digit = faktorStr1 + faktorStr2
+      //console.log(digit)
+
+      if (lowest ===0){
+        lowest = digit;
+      } else if (digit < lowest){
+        lowest = digit
+      }
+    }
+    return lowest
   }
   
   // TEST CASES
